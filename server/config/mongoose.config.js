2221b66e5884aaf1d6fde33f12e45cae5e8622cc
Fernanda Monroe <inandamonroe@gmail.com>
2024-03-21 15:14:43 -0300

    Update MongoDB connection URI

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,11 +1,11 @@
 // ----CONFIG---
 
 const mongoose = require('mongoose');
-const dbName = process.env.DB;
-const username = process.env.ATLAS_USERNAME;
-const pw = process.env.ATLAS_PASSWORD;
-const uri = `mongodb+srv://${username}:${pw}@cluster0.fnsmyj3.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
+// const dbName = process.env.DB;
+// const username = process.env.ATLAS_USERNAME;
+// const pw = process.env.ATLAS_PASSWORD;
+// const uri = `mongodb+srv://${username}:${pw}@cluster0.fnsmyj3.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
 
-mongoose.connect(uri)
+mongoose.connect(process.env.DB_URI)
     .then(() => console.log("📡📡📡📡 Established a connection to the database"))
     .catch(err => console.log("❌❌❌❌ Something went wrong when connecting to the database", err));
